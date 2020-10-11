be54f380f3b23c0e24a75a5b0abce8c6019afccf
David Lockwood <dave.r.lockwood@gmail.com>
2020-10-10 20:53:21 -0400

    fixes

--- osp-config.sh
+++ osp-config.sh
@@ -230,7 +230,7 @@ install_osp_rtmp() {
   sudo cp $DIR/installs/osp-rtmp/setup/nginx/servers/*.conf /usr/local/nginx/conf/servers
   sudo cp $DIR/installs/osp-rtmp/setup/nginx/services/*.conf /usr/local/nginx/conf/services
   sudo mkdir /opt/osp-rtmp
-  sudo cp -R $DIR/installs/osp-rtmp/* /osp/osp-rtmp
+  sudo cp -R $DIR/installs/osp-rtmp/* /opt/osp-rtmp
   sudo cp $DIR/installs/osp-rtmp/setup/gunicorn/osp-rtmp.service /etc/systemd/system/osp-rtmp.service
   sudo systemctl daemon-reload
   sudo systemctl enable osp-rtmp.service
