0ec2a846638438c65e84565660566cd69bcc8ef4
Deamos <dave.r.lockwood@gmail.com>
2020-12-07 17:54:20 -0500

    Readding

---
+++ installs/osp-rtmp/blueprints/root.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, url_for, render_template, redirect, current_app, send_from_directory, abort, flash
+
+root_bp = Blueprint('root', __name__)
+
+@root_bp.route('/playbackAuth', methods=['POST'])
+def playback_auth_handler():
+    stream = request.form['name']
+    clientIP = request.form['addr']
+
+    if clientIP == "127.0.0.1" or clientIP == "localhost":
+        return 'OK'
+    else:
+        return abort(400)
