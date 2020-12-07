cbfbf5e36d551197e82c43ddf827979d3fe3803d
Deamos <deamosseraph@hotmail.com>
2020-12-07 13:41:56 -0500

    Fix to debug

--- installs/osp-rtmp/blueprints/rtmp.py
+++ installs/osp-rtmp/blueprints/rtmp.py
@@ -92,7 +92,7 @@ def user_auth_check():
                             else:
                                 subprocessConstructor.append("rtmp://" + node['address'] + "/stream-data/" + channelLocation)
 
-                            print("Open ffmpeg subprocess node - " + subprocessConstructor)
+                            print("Open ffmpeg subprocess node - " + str(subprocessConstructor))
                             p = subprocess.Popen(subprocessConstructor, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                             globalvars.edgeRestreamSubprocesses[channelLocation].append(p)
                 return 'OK'
